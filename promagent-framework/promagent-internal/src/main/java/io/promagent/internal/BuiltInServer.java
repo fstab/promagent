e4f1ffe9db20c975d13f8dfcaba3338604222504
Fabian Stäber <fabian@fstab.de>
2018-01-12 13:51:35 +0100

    increase usability of built-in http server

--- promagent-framework/promagent-internal/src/main/java/io/promagent/internal/BuiltInServer.java
+++ promagent-framework/promagent-internal/src/main/java/io/promagent/internal/BuiltInServer.java
@@ -14,10 +14,12 @@
 
 package io.promagent.internal;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.common.TextFormat;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.net.InetSocketAddress;
 import java.util.Collections;
@@ -32,18 +34,36 @@ class BuiltInServer {
             int port = Integer.parseInt(portString);
             InetSocketAddress address = host == null ? new InetSocketAddress(port) : new InetSocketAddress(host, port);
             HttpServer httpServer = HttpServer.create(address, 10);
-            httpServer.createContext("/metrics", httpExchange -> {
-                StringWriter respBodyWriter = new StringWriter();
-                TextFormat.write004(respBodyWriter, registry.metricFamilySamples());
-                byte[] respBody = respBodyWriter.toString().getBytes("UTF-8");
-                httpExchange.getResponseHeaders().put("Context-Type", Collections.singletonList("text/plain; charset=UTF-8"));
-                httpExchange.sendResponseHeaders(200, respBody.length);
-                httpExchange.getResponseBody().write(respBody);
-                httpExchange.getResponseBody().close();
+            httpServer.createContext("/", httpExchange -> {
+                if ("/metrics".equals(httpExchange.getRequestURI().getPath())) {
+                    respondMetrics(registry, httpExchange);
+                } else {
+                    respondRedirect(httpExchange);
+                }
             });
             httpServer.start();
         } catch (NumberFormatException e) {
             throw new RuntimeException("Failed to parse command line arguments: '" + portString + "' is not a valid port number.");
         }
+    }
+
+    private static void respondMetrics(CollectorRegistry registry, HttpExchange httpExchange) throws IOException {
+        StringWriter respBodyWriter = new StringWriter();
+        respBodyWriter.write("# Metrics will become visible when they are updated for the first time.\n");
+        TextFormat.write004(respBodyWriter, registry.metricFamilySamples());
+        byte[] respBody = respBodyWriter.toString().getBytes("UTF-8");
+        httpExchange.getResponseHeaders().put("Context-Type", Collections.singletonList("text/plain; charset=UTF-8"));
+        httpExchange.sendResponseHeaders(200, respBody.length);
+        httpExchange.getResponseBody().write(respBody);
+        httpExchange.getResponseBody().close();
+    }
+
+    private static void respondRedirect(HttpExchange httpExchange) throws IOException {
+        byte[] respBody = "Metrics are provided on the /metrics endpoint.".getBytes("UTF-8");
+        httpExchange.getResponseHeaders().add("Location", "/metrics");
+        httpExchange.getResponseHeaders().put("Context-Type", Collections.singletonList("text/plain; charset=UTF-8"));
+        httpExchange.sendResponseHeaders(302, respBody.length);
+        httpExchange.getResponseBody().write(respBody);
+        httpExchange.getResponseBody().close();
     }
 }
